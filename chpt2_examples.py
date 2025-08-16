b13adf34f9d09e63f2e564cc75993b80137756ca
Daniel Soriano <danielsoriano559@gmail.com>
2025-08-16 15:10:42 -0700

    add chapt2_examples.py

---
+++ chpt2_examples.py
@@ -0,0 +1,106 @@
+# Print 4 to console
+print(4)
+
+# Getting type's of values
+type('Hello, World!')
+type(17)
+type(3.2)
+type('17')
+type('3.2')
+
+# Print 1 0 0 to console
+print(1,000,000)
+
+# Define variables, print out values, and get type of values
+message = 'And now for something completely different'
+n = 17
+pi = 3.1415926535897931
+print(n)
+print(pi)
+type(message)
+type(n)
+type(pi)
+
+# Illegal Names
+# 76trombones = 'big parade'
+# more@ = 1000000
+# class = 'Advanced Theoretical Zymurgy'
+
+print(1)
+x = 2
+print(x)
+
+minute = 59
+print(minute/60)
+print(minute//60)
+
+# EXPRESSIONS
+# 17
+# x
+# x + 17
+# 1+1
+# 5
+# x = 5
+# x + 1
+
+# Modulus operator
+quotient = 7 // 3
+print(quotient)
+remainder = 7 % 3
+print(remainder)
+
+# String operations
+first = 10
+second = 15
+print(first+second)
+first = '100'
+second = '150'
+print(first + second)
+
+# User input
+inp = input()
+print(inp)
+name = input('What is your name?\n')
+print(name)
+
+prompt = 'What...is the airspeed velocity of an unladen swallow?\n'
+speed = input(prompt)
+int(speed)
+int(speed) + 5
+
+speed = input(prompt)
+int(speed)
+
+# Comments begin with hash
+
+
+# Choosing mnemonic variable names
+a = 35.0
+b = 12.50
+c = a * b
+print(c)
+
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print(pay)
+
+x1q3z9ahd = 35.0
+x1q3z9afd = 12.50
+x1q3p9afd = x1q3z9ahd * x1q3z9afd
+print(x1q3p9afd)
+
+# Loops
+words = ['some', 'random', 'words']
+for word in words:
+    print(word)
+
+pizza = ['slice', 'slice', 'slice', 'slice']
+for slice in pizza:
+    print(slice)
+
+# Debugging
+# bad name = 5
+# principal = 327.68
+# interest = principle * rate
+# 1.0 / 2.0 * pi
