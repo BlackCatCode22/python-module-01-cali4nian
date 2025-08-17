0380a70ba96e7075a98eea97f126e292706c382d
Daniel Soriano <danielsoriano559@gmail.com>
2025-08-17 10:39:45 -0700

    add chpt2_excersises.py

---
+++ chpt2_exercises.py
@@ -0,0 +1,43 @@
+# Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+# Enter your name: Chuck
+# Hello Chuck
+# print("Enter your name: ")
+# userName = input()
+# print('Hello ' + userName)
+
+# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+# Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+# We won’t worry about making sure our pay has exactly two digits after the decimal place for now. If you want, you can play with the built-in Python round function to properly round the resulting pay to two decimal places.
+# print('Enter Hours')
+# hours = float(input())
+# print('Enter Rate')
+# rate = float(input())
+# print('Your payout is ' + str(hours * rate))
+
+# Exercise 4: Assume that we execute the following assignment statements:
+# width = 17
+# height = 12.0
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression).
+#
+# width//2
+#
+# width/2.0
+#
+# height/3
+#
+# 1 + 2 * 5
+# 1) 8
+# Use the Python interpreter to check your answers.
+
+# 2) 8.5
+# 3) 4.0
+# 4) 11
+# print(1 + 2 * 5)
+
+# Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+# print('Enter a Temperature in Celsius:')
+# celsius = float(input())
+# fahrenheit = 9/5 * celsius + 32
+# print('Temperature in Fahrenheit:', fahrenheit)
