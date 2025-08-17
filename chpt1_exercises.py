084b3c88699d7a1e6b932fa2609a3c9d3b2c0f93
Daniel Soriano <danielsoriano559@gmail.com>
2025-08-17 10:23:50 -0700

    add chpt1_exercises.py

---
+++ chpt1_exercises.py
@@ -0,0 +1,44 @@
+# Exercise 1: What is the function of the secondary memory in a computer?
+# c) Store information for the long term, even beyond a power cycle
+
+# Exercise 2: What is a program?
+# A program is a set of instructions the computer understands that solves
+# a problem for the end user.
+
+# Exercise 3: What is the difference between a compiler and an interpreter?
+# A compiler needs to be handed the entire program in a file, and then
+# it runs a process to translate the high-level source code into machine language
+# in a file for later execution. An interpreter allows code to be more portable
+# with other machines with different hardware and software.
+
+# Exercise 4: Which of the following contains “machine code”?
+# a) The Python interpreter
+
+# Exercise 5: What is wrong with the following code:
+
+# >>> primt 'Hello world!'
+# File "<stdin>", line 1
+#   primt 'Hello world!'
+#                      ^
+# SyntaxError: invalid syntax
+# >>>
+
+# Answer Exercise 5: the command is misspelled and missing key syntax errors.
+# Correct version is print('Hello world!')
+
+
+# Exercise 6: Where in the computer is a variable such as “x” stored after the following Python line finishes?
+# x = 123
+# b) Main Memory
+
+# Exercise 7: What will the following program print out:
+# x = 43
+# x = x - 1
+# print(x)
+# b) 42
+
+# Exercise 8: Explain each of the following using an example of a human capability: (1) Central processing unit, (2) Main Memory, (3) Secondary Memory, (4) Input Device, and (5) Output Device. For example, “What is the human equivalent to a Central Processing Unit”?
+# CPU processes data for output or result, Main memory holds what is next
+# for a CPU to process, Secondary Memory is used for long time storage, Input
+# Device are devices such as keyboard, mouse, and microphone, Output Device are devices
+# such as printer, screens, and audio speakers.
